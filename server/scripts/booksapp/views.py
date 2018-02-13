ea419e1383412720149bb5553bd56280f058d6da
Dmitry <dgdagadin87@mail.ru>
2018-02-13 18:02:37 +0300

    Csrf was removed

--- server/scripts/booksapp/views.py
+++ server/scripts/booksapp/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from .include.miscutils.sessions import BooksSessions
 from .include.controllers.gui.central import gui_central_controller
 from .include.controllers.api.login import api_login_controller
@@ -7,5 +8,6 @@ def gui_central(request):
     return gui_central_controller(BooksSessions, request)
 
 
+@csrf_exempt
 def api_login(request):
     return api_login_controller(BooksSessions, request)
