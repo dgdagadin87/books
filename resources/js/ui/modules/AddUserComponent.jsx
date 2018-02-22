a2f2163d85cbfd16b7b6caa2ffe1ef1838175677
Dmitry <dgdagadin87@mail.ru>
2018-02-22 13:28:30 +0300

    SPA: Add user component was modified: error handling

--- resources/js/ui/modules/AddUserComponent.jsx
+++ resources/js/ui/modules/AddUserComponent.jsx
@@ -91,6 +91,7 @@ class AddUserComponent extends BaseModule {
                     let {data = {}} = result;
                     
                     if (data.hasError === true) {
+                        globalEvents.trigger('showError', result);
                         this.setStats({
                             errorText: data.errorText,
                             disabled: false
