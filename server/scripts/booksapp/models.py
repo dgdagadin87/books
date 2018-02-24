075f766702a8c1cdc4a8c199934d389a30286515
Dmitry <dgdagadin87@mail.ru>
2018-02-24 16:28:20 +0300

    Model migrations...

--- server/scripts/booksapp/models.py
+++ server/scripts/booksapp/models.py
@@ -7,6 +7,12 @@ class Settings(models.Model):
 	setting_value = models.TextField()
 
 
+class Sites(models.Model):
+	site_id = models.AutoField(primary_key=True)
+	site_name = models.CharField(max_length=100, unique=True)
+	site_url = models.CharField(max_length=200)
+
+
 class Users(models.Model):
 	user_id = models.AutoField(primary_key=True)
 	user_login = models.CharField(max_length=100,unique=True)
@@ -18,21 +24,6 @@ class Users(models.Model):
 	user_is_admin = models.CharField(max_length=3, choices=IS_ADMIN_CHOICES, default='no')
 	user_password = models.CharField(max_length=100)
 	user_secret_key = models.CharField(max_length=100, default='qwerty')
-
-
-class Books_2_users(models.Model):
-	books_2_users_id = models.AutoField(primary_key=True)
-	book_id = models.BigIntegerField()
-	user_id = models.BigIntegerField()
-
-	class Meta:
-		unique_together = ('book_id', 'user_id')
-
-
-class Sites(models.Model):
-	site_id = models.AutoField(primary_key=True)
-	site_name = models.CharField(max_length=100, unique=True)
-	site_url = models.CharField(max_length=200)
 
 
 class Cached_books(models.Model):
@@ -48,11 +39,32 @@ class Books(models.Model):
 	book_genre = models.CharField(max_length=200)
 	book_short_desc = models.TextField()
 	book_size = models.BigIntegerField()
-	parent_site_id = models.IntegerField()
-	cached_book_id = models.BigIntegerField()
-	
+	parent_site_id = models.ForeignKey(
+		Sites,
+		on_delete=models.CASCADE
+	)
+	cached_book_id = models.ForeignKey(
+		Cached_books,
+		on_delete=models.CASCADE
+	)
+
 	class Meta:
 		unique_together = (('book_id', 'parent_site_id'), ('book_id', 'cached_book_id'))
+
+
+class Books_2_users(models.Model):
+	books_2_users_id = models.AutoField(primary_key=True)
+	book_id = models.ForeignKey(
+		Books,
+		on_delete=models.CASCADE
+	)
+	user_id = models.ForeignKey(
+		Users,
+		on_delete=models.CASCADE
+	)
+
+	class Meta:
+		unique_together = ('book_id', 'user_id')
 
 
 class Headers(models.Model):
